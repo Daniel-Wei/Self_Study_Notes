db48af8ceccb128bd96a1f4a362a6bcf5e638847
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2025-08-02 16:02:45 +0800

    Update and rename SimpleComponent.js to SimpleFunctionComponent.js

--- Frontend/JS/React/React18_Bilibili/React Mini Project/001. 组件/函数式组件/SimpleComponent.js
+++
@@ -1,5 +0,0 @@
-const SimpleComponent = () => {
-    return <div>我是简单的函数式组件</div>
-}
-
-export default SimpleComponent;
---
+++ Frontend/JS/React/React18_Bilibili/React Mini Project/001. 组件/函数式组件/SimpleFunctionComponent.js
@@ -0,0 +1,5 @@
+const SimpleFunctionComponent = () => {
+    return <div>我是简单的函数式组件</div>
+}
+
+export default SimpleFunctionComponent;
