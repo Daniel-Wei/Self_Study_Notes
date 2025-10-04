eb9932b273a0630c132a10692b85d16e999f87f7
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2025-10-05 03:54:45 +1100

    Update 1. Problem_1.js

--- Frontend/JS/React/React18_Bilibili/Hooks/5. useMemo()/2. Preventing an Effect from firing too often/1. Problem_1.js
+++ Frontend/JS/React/React18_Bilibili/Hooks/5. useMemo()/2. Preventing an Effect from firing too often/1. Problem_1.js
@@ -13,21 +13,3 @@ function ChatRoom({ roomId }) {
       // ...
     }); 
 };
-
-// Solution 2: No need for useMemo. As React may throw away the cached value if there is a specific reason to do that. 
-function ChatRoom({ roomId }) {
-    const [message, setMessage] = useState('');
-  
-    useEffect(() => {
-        const options = { // ✅ No need for useMemo or object dependencies!
-            serverUrl: 'https://localhost:1234',
-            roomId: roomId
-        }
-    
-        const connection = createConnection(options);
-        connection.connect();
-        return () => connection.disconnect();
-    }, [roomId]); // ✅ Only changes when roomId changes
-    // ...
-};
-
