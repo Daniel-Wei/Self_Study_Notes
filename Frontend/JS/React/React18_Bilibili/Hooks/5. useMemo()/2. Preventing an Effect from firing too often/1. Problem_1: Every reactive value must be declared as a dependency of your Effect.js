ce454a0ed729363e0b449c17e2da67763b22fc05
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2025-10-05 03:52:30 +1100

    Create 1. Problem_1: Every reactive value must be declared as a dependency of your Effect.js

---
+++ Frontend/JS/React/React18_Bilibili/Hooks/5. useMemo()/2. Preventing an Effect from firing too often/1. Problem_1: Every reactive value must be declared as a dependency of your Effect.js
@@ -0,0 +1,69 @@
+// problem 1:  
+function ChatRoom({ roomId }) {
+    const [message, setMessage] = useState('');
+  
+    const options = {
+      serverUrl: 'https://localhost:1234',
+      roomId: roomId
+    }
+
+    useEffect(() => {
+      const connection = createConnection(options);
+      connection.connect();
+      // ...
+    }); 
+};
+
+// problem 2: if declare options as Effect's dependency, every re-render will trigger useEffect() once again
+function ChatRoom({ roomId }) {
+    const [message, setMessage] = useState('');
+  
+    const options = {
+        serverUrl: 'https://localhost:1234',
+        roomId: roomId
+    }
+
+    useEffect(() => {
+        const connection = createConnection(options);
+        connection.connect();
+        return () => connection.disconnect();
+    }, [options]); // 🔴 Problem: This dependency changes on every render
+});
+
+// Solution 1:  Wrap the object you need to call from an Effect in useMemo
+function ChatRoom({ roomId }) {
+    const [message, setMessage] = useState('');
+
+    // This ensures that the options object is the same between re-renders if useMemo returns the cached object.
+    const options = useMemo(() => {
+        return {
+            serverUrl: 'https://localhost:1234',
+            roomId: roomId
+        };
+    }, [roomId]); // ✅ Only changes when roomId changes
+  
+    useEffect(() => {
+          const connection = createConnection(options);
+          connection.connect();
+          return () => connection.disconnect();
+    }, [options]); // ✅ Only changes when options changes
+    // ...
+};
+
+// Solution 2: No need for useMemo. As React may throw away the cached value if there is a specific reason to do that. 
+function ChatRoom({ roomId }) {
+    const [message, setMessage] = useState('');
+  
+    useEffect(() => {
+        const options = { // ✅ No need for useMemo or object dependencies!
+            serverUrl: 'https://localhost:1234',
+            roomId: roomId
+        }
+    
+        const connection = createConnection(options);
+        connection.connect();
+        return () => connection.disconnect();
+    }, [roomId]); // ✅ Only changes when roomId changes
+    // ...
+};
+
