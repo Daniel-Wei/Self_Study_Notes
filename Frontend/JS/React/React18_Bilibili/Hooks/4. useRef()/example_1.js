94258c6ea48a3048490a3466f7d0c97c45e69a60
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2025-10-04 23:35:06 +1000

    Create example_1.js

---
+++ Frontend/JS/React/React18_Bilibili/Hooks/4. useRef()/example_1.js
@@ -0,0 +1,33 @@
+import { useState, useRef, useEffect } from 'react';
+
+function VideoPlayer({ src, isPlaying }) {
+    const ref = useRef(null);
+
+    // why useEffect?
+    // 1. When <VideoPlayer/> rendered, ref is still null -> cannot read ref.current
+    // 2. utilize useEffect(), it will be called *after* <VideoPlayer/> rendered -> ref.current is <video/>
+    useEffect(() => {
+        if (isPlaying) {
+            ref.current.play();
+        } else {
+            ref.current.pause();
+        }
+    });
+  
+    return <video ref={ref} src={src} loop playsInline />;
+}
+
+export default function App() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    return (
+        <>
+            <button onClick={() => setIsPlaying(!isPlaying)}>
+                {isPlaying ? '暂停' : '播放'}
+            </button>
+            <VideoPlayer
+                isPlaying={isPlaying}
+                src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+            />
+        </>
+    );
+}
