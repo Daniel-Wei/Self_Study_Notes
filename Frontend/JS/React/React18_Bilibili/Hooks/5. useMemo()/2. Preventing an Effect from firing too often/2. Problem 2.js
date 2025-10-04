98d5ccf20871177b34e83291d74b82ab58cd7732
Daniel-Wei <49779031+Daniel-Wei@users.noreply.github.com>
2025-10-05 03:53:42 +1100

    Update 2. Problem 2.js

--- Frontend/JS/React/React18_Bilibili/Hooks/5. useMemo()/2. Preventing an Effect from firing too often/2. Problem 2.js
+++ Frontend/JS/React/React18_Bilibili/Hooks/5. useMemo()/2. Preventing an Effect from firing too often/2. Problem 2.js
@@ -1,1 +1,16 @@
-problem 2: if declare options as Effect's dependency, every re-render will trigger useEffect() once again
+// problem 2: if declare options as Effect's dependency, every re-render will trigger useEffect() once again
+
+function ChatRoom({ roomId }) {
+    const [message, setMessage] = useState('');
+  
+    const options = {
+        serverUrl: 'https://localhost:1234',
+        roomId: roomId
+    }
+
+    useEffect(() => {
+        const connection = createConnection(options);
+        connection.connect();
+        return () => connection.disconnect();
+    }, [options]); // 🔴 Problem: This dependency changes on every render
+});
